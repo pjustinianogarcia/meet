5df4568a047ee553a206e35259979646edbcf3fb
pjustinianogarcia <158845576+pjustinianogarcia@users.noreply.github.com>
2024-07-19 18:11:13 -0400

    Added integration test

--- src/App.js
+++ src/App.js
@@ -4,18 +4,36 @@ import NumberOfEvents from "./components/NumberOfEvents";
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import mockData from './mock-data'; 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { extractLocations, getEvents } from './api';
 import './App.css';
 
 
 const App = () => {
+  const [events, setEvents] = useState([]);
   const [numberOfEvents, setNumberOfEvents] = useState(32);
+  const [allLocations, setAllLocations] = useState([]);
+  const [currentCity, setCurrentCity] = useState("See all cities");
+  
+
+  useEffect(() => {
+    fetchData();
+  }, [currentCity, numberOfEvents]);
+
+  const fetchData = async () => {
+    const allEvents = await getEvents();
+    const filteredEvents = currentCity === "See all cities" ?
+      allEvents :
+      allEvents.filter(event => event.location === currentCity)
+    setEvents(filteredEvents.slice(0, numberOfEvents));
+    setAllLocations(extractLocations(allEvents));
+  }
 
  return (
    <div className="App">
-    <CitySearch />
+    <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity}/>
     <NumberOfEvents setNumberOfEvents={setNumberOfEvents} />
-    <EventList events={mockData.slice(0, numberOfEvents)} />
+    <EventList events={events} />
    </div>
  );
 }
--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,6 +1,8 @@
 // src/__tests__/App.test.js
 
-import { render } from '@testing-library/react';
+import { render, within, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { getEvents } from '../api';
 import App from '../App';
 
 describe('<App /> component', () => {
@@ -16,4 +18,53 @@ describe('<App /> component', () => {
   test('render CitySearch', () => {
     expect(AppDOM.querySelector('#city-search')).toBeInTheDocument();
   });
+});
+
+describe('<App /> integration', () => {
+  test('renders a list of events matching the city selected by the user', async () => {
+    const user = userEvent.setup();
+    const AppComponent = render(<App />);
+    const AppDOM = AppComponent.container.firstChild;
+
+    const CitySearchDOM = AppDOM.querySelector('#city-search');
+    const CitySearchInput = within(CitySearchDOM).queryByRole('textbox');
+
+    await user.type(CitySearchInput, "Berlin");
+    const berlinSuggestionItem = within(CitySearchDOM).queryByText('Berlin, Germany');
+    await user.click(berlinSuggestionItem);
+
+    const EventListDOM = AppDOM.querySelector('#event-list');
+    
+    const allRenderedEventItems = within(EventListDOM).queryAllByRole('listitem');   
+
+    const allEvents = await getEvents();
+    const berlinEvents = allEvents.filter(
+      event => event.location === 'Berlin, Germany'
+    );
+
+    expect(allRenderedEventItems.length).toBe(berlinEvents.length);
+
+    allRenderedEventItems.forEach(event => {
+      expect(event.textContent).toContain("Berlin, Germany");
+    });
+  });
+
+  test('renders a list of events matching the number of events input by the user', async () => {
+    const user = userEvent.setup();
+    const AppComponent = render(<App />);
+    const AppDOM = AppComponent.container.firstChild;
+
+    const NumberOfEventsDOM = AppDOM.querySelector('.number-of-events');
+    const NumberOfEventsInput = within(NumberOfEventsDOM).queryByRole('textbox');
+
+    await user.type(NumberOfEventsInput, "{backspace}{backspace}10");
+
+    const EventListDOM = AppDOM.querySelector('#event-list');
+    await waitFor(() => {
+      const EventListItems = within(EventListDOM).queryAllByRole('listitem');
+      expect(EventListItems.length).toBe(10);
+    });
+  });
+
+
 });
