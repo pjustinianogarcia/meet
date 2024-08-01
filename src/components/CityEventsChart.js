985d5ea71a72457bb03f32ebee32b06a150f7f19
pjustinianogarcia <158845576+pjustinianogarcia@users.noreply.github.com>
2024-08-01 16:12:23 -0400

    Fixed the city names display on scatterchart

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -20,8 +20,8 @@ const CityEventsChart = ({ allLocations, events }) => {
     const getData = () => {
         const data = allLocations.map((location) => {
         const count = events.filter((event) => event.location === location).length
-          const city = location.split(', ')[0]
-          return { count, count };
+          const city = location.split((/, | - /))[0];
+          return { city, count };
         })
         return data;
       };
@@ -36,10 +36,10 @@ const CityEventsChart = ({ allLocations, events }) => {
             }}
           >
             <CartesianGrid />
-            <XAxis type="category" dataKey="city" name="City" />
-            <YAxis type="number" dataKey="count" name="number of events" />
+            <XAxis type="category" dataKey="city" name="City" angle={60} interval={0} tick={{ dx: 20, dy: 40, fontSize: 14 }} />
+            <YAxis type="number" dataKey="count" name="number of events" allowDecimals={false} />
             <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-            <Scatter name="A school" data={data} fill="#8884d8" />
+            <Scatter name="Events" data={data} fill="#8884d8" />
           </ScatterChart>
         </ResponsiveContainer>
       );
